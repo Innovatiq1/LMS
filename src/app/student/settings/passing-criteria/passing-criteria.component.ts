8dad3d5759136c1e5578f5294cec25c06b6525ea
PremkumarInnovatiq <premkumar.vudatha@innovatiqconsulting.com>
2024-06-21 17:19:23 +0530

    passing_critera

---
+++ src/app/student/settings/passing-criteria/passing-criteria.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passing-criteria',
+  templateUrl: './passing-criteria.component.html',
+  styleUrls: ['./passing-criteria.component.scss']
+})
+export class PassingCriteriaComponent {
+
+}
