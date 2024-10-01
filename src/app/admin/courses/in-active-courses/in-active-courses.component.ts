097ebfe838e86b1883f2b218b9df1c0089687e65
prosanjeet1998 <prosanjeet@innovatiqconsulting.com>
2024-10-01 09:26:08 +0530

    TMS Course and Program Module Bugs

--- src/app/admin/courses/in-active-courses/in-active-courses.component.ts
+++ src/app/admin/courses/in-active-courses/in-active-courses.component.ts
@@ -151,6 +151,7 @@ export class InActiveCoursesComponent {
       .getAllCourses(userId,{ ...this.coursePaginationModel, status: 'inactive' })
       .subscribe(
         (response) => {
+          console.log("this.dataSource",response.data.docs)
           this.isLoading = false;
           this.coursePaginationModel.docs = response.data.docs;
           this.coursePaginationModel.page = response.data.page;
