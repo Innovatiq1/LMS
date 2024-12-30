f0bfdc408f3abba57809ea290e4e0588287bcf7c
Gowtham Ganesan <mail2gowthamganesan@gmail.com>
2024-12-30 21:49:21 +0530

    activity log added

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -3,9 +3,10 @@ import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { SharedModule } from '../shared.module';
 import { TestPreviewComponent } from './test-preview/test-preview.component';
 import { StaticBreadcrumbComponent } from './static-breadcrumb/static-breadcrumb.component';
+import { ActivityLogComponent } from './activity-log/activity-log.component';
 
 @NgModule({
-  declarations: [ BreadcrumbComponent, TestPreviewComponent, StaticBreadcrumbComponent],
+  declarations: [ BreadcrumbComponent, TestPreviewComponent, StaticBreadcrumbComponent, ActivityLogComponent],
   imports: [SharedModule],
   exports: [BreadcrumbComponent,StaticBreadcrumbComponent],
 })
