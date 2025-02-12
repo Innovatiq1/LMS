be1c85a7071e4c38b15698feff8b0ded7d9d1ef6
Gowtham Ganesan <mail2gowthamganesan@gmail.com>
2025-02-12 15:31:33 +0530

    Revert "proctoring hide"
    This reverts commit 74b8eac6fdb7aa746d762f99accce5ccde467125.

--- src/app/student/settings/add-exam-questions/add-exam-questions.component.ts
+++ src/app/student/settings/add-exam-questions/add-exam-questions.component.ts
@@ -86,7 +86,7 @@ export class AddExamQuestionsComponent implements OnInit, OnDestroy{
       timer: [],
       retake:[],
       passingCriteria:['', Validators.required],
-      // videoAnalyzerReq: [false, Validators.required],
+      videoAnalyzerReq: [false, Validators.required],
       scoreAlgorithm:[, [Validators.required,Validators.min(0.1)]],
       questions: this.formBuilder.array([]),
     });
@@ -164,7 +164,7 @@ saveDraft(data?: string) {
           retake: this.questionFormTab2.value.retake,
           passingCriteria:this.questionFormTab2.value.passingCriteria,
           scoreAlgorithm: this.questionFormTab2.value.scoreAlgorithm,
-          videoAnalyzerReq: false,
+          videoAnalyzerReq: this.questionFormTab2.value.videoAnalyzerReq,
           status: 'draft',
           companyId:userId,
           questions: this.questionFormTab2.value.questions.map((v: any) => ({
@@ -242,6 +242,7 @@ saveDraft(data?: string) {
               passingCriteria:String(response?.passingCriteria),
               retake:String(response?.retake),
               scoreAlgorithm:response?.scoreAlgorithm,
+              videoAnalyzerReq: response?.videoAnalyzerReq,
               timer:response?.timer
             });
 
@@ -399,7 +400,7 @@ saveDraft(data?: string) {
         retake: this.questionFormTab2.value.retake,
         passingCriteria:this.questionFormTab2.value.passingCriteria,
         scoreAlgorithm: this.questionFormTab2.value.scoreAlgorithm,
-        videoAnalyzerReq: false,
+        videoAnalyzerReq: this.questionFormTab2.value.videoAnalyzerReq,
         status:this.dialogStatus?'approved':'open',
         companyId:userId,
         questions: this.questionFormTab2.value.questions.map((v: any) => ({
