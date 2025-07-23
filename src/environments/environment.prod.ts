dac14d408801fb209334c2cd3467bd66f7fa4550
GaneshInnovatiq <ganesh.masilamani@innovatiqconsulting.com>
2025-07-23 22:28:48 +0530

    UAT env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,13 @@
- 
+export const environment = {
+  production: true,
+  apiUrl: 'http://uatskillera.innovatiqconsulting.com/api/',
+  Url: 'http://uatskillera.innovatiqconsulting.com/',
+  companyUrl: 'http://uatskillera.innovatiqconsulting.com/',
+  publicApiUrl: 'http://uatskillera.innovatiqconsulting.com/x-api/v1/public/',
+  apiEndpointNew: 'http://uatskillera.innovatiqconsulting.com/x-api/v1/',
+  frontEndUrl: 'http://uatskillera.innovatiqconsulting.com/',
+  apiNewEndpoint: 'http://uatskillera.innovatiqconsulting.com/api1/',
+  awsUrl: 'https://collegey.s3.ap-southeast-1.amazonaws.com/',
+  filesPath:'https://s3.ap-southeast-1.amazonaws.com/storage.collegey/',
+  ZoomUrl:'https://zoom.us/oauth/authorize?client_id=LiiBCSVsQzi2MPZIsglZYg&response_type=code&redirect_uri=http%3A%2F%2Fuatskillera.innovatiqconsulting.com%2Fadmin%2Fcourses%2Fcreate-class'
+};
